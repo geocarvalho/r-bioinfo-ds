a12d52583722a123162cbfd0118dcd52f7bcdb1b
geocarvalho <geocarvalho@outlook.com>
2017-05-28 22:29:47 -0300

    started week2

---
+++ r-programing/week2/week2.R
@@ -0,0 +1,122 @@
+# Control Structures
+# if, else: testing a condition
+# for: execute a loop a fixed number of times
+# while: execute a loop while a condition is true
+# repeat: execute an infinite loop
+# break: break the execution of a loop
+# next: skip an interation of a loop
+# return: exit a function
+
+# If/else
+x=3
+y <- if(x>3) {
+  10
+} else {
+  0
+}
+y
+
+# For loops
+for(i in 1:10) {
+  print(i)
+}
+x <- c("a", "b", "c", "d")
+for(i in 1:4) {
+  print(x[i])
+}
+for(i in seq_along(x)) {
+  print(x[i])
+}
+for(letter in x) {
+  print(letter)
+}
+for(i in 1:4) print(x[i])
+# nested for loops
+x <- matrix(1:6, 2, 3)
+for(i in seq_len(nrow(x))) {
+  for(j in seq_len(ncol((x)))) {
+    print(x[i, j])
+  }                      
+}
+x
+
+# While
+count <- 0
+while(count < 10) {
+  print(count)
+  count <- count+1
+}
+
+z<- 5
+while(z >= 3 && z <= 10) {
+  print(z)
+  coin <- rbinom(1, 1, 0.5)
+  if(coin == 1) {
+    z <- z+1
+  } else {
+    z <- z-1
+  }
+}
+
+# Repeat and break
+x0 <- 1
+tol <- 1e-8
+repeat {
+  x1 <- computeEstimate() # isn't real
+  if(abs(x1-x0) < tol) {
+    break
+  } else {
+    x0 <- x1
+  }
+}
+
+# Next and return
+for(i in 1:100) {
+  if(i <= 20) {
+    next
+  } else {
+    print("Hello world")
+  }
+}
+
+# Functions
+add2 <- function(x, y) {
+  x+y
+}
+add2(3, 2)
+
+above10 <- function(x) {
+  use <- x > 10
+  x[use]
+}
+
+above <- function(x, n=10) {
+  use <- x > n
+  x[use]
+}
+x <- 1:20
+above(x)
+
+columnmean <- function(y, removeNA=TRUE) {
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc) {
+    means[i] <- mean(y[, i], na.rm=removeNA) #calculate for the entire column
+  }
+  means
+}
+columnmean(airquality)
+columnmean(airquality, FALSE)
+
+# Functions part 1
+# argument matching
+mydata <- rnorm(100)
+sd(mydata)
+sd(x=mydata)
+sd(x=mydata, na.rm=FALSE)
+sd(na.rm=FALSE, x=mydata)
+sd(na.rm=FALSE, mydata)
+
+args(lm)
+lm(data = mydata, y ~ x, model = FALSE, 1:100)
+lm(y ~ x, mydata, 1:100, model=FALSE)
