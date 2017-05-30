3601dcc236da6c3e9ccc2ce91418667eae2e2121
geocarvalho <geocarvalho@outlook.com>
2017-05-29 22:54:34 -0300

    so tired

--- r-programing/week2/week2.R
+++ r-programing/week2/week2.R
@@ -120,3 +120,101 @@ sd(na.rm=FALSE, mydata)
 args(lm)
 lm(data = mydata, y ~ x, model = FALSE, 1:100)
 lm(y ~ x, mydata, 1:100, model=FALSE)
+
+# Functions part 2
+#lazy evaluation
+f <- function(a, b) {
+  a+2
+}
+f(2)
+f <- function(a, b) {
+  print(a)
+  print(b)
+}
+f(45)
+# the "..." argument
+myplot <- function(x, y, type="1", ...) {
+  plot(x, y, type=type, ...)
+}
+
+args(paste)
+args(cat)
+
+paste("a", "b", sep=":")
+
+# Scoping rules
+lm <- function(x) { x*x }
+lm
+# lexical scoping
+f <- function(x, y) {
+  x**2 + y/z
+}
+
+make.power <- function(n) {
+  pow <- function(x) {
+    x**n
+  }
+  pow
+}
+cube <- make.power(3)
+square <- make.power(2)
+cube(3)
+square(3)
+
+# function closure
+ls(environment(cube))
+get("n", environment(cube))
+ls(environment(square))
+get("n", environment(square))
+
+# lexical vs dynamic scoping
+y <- 10
+f <- function(x) {
+  y <- 2 #lexical scoping
+  y**2 + g(x)
+}
+g <- function(x) {
+  x*y #dynamic scoping (variable environment)
+}
+f(3)
+
+g <- function(x) {
+  a <- 3
+  x+a+y
+}
+g(2)
+y <- 3
+g(2)
+
+# Scoping rules - optimization example
+# maximizing a normal likelihood
+make.NegLogLik <- function(data, fixed=c(FALSE, FALSE)) {
+  params <- fixed
+  function(p) {
+    params[!fixed] <- p
+    mu <- params[1]
+    sigma <- params[2]
+    a <- -0.5*length(data)*log(2*pi*sigma**2)
+    b <- -0.5*sum(data-mu)**2) / (sigma**2)
+    -(a+b)
+  }
+}
+set.seed(1); normals <- rnorm(100, 1, 2)
+nLL <- make.NegLogLik(normals)
+nLL
+ls(environment(nLL))
+# estimating parameters
+optim(c(mu=0, sigma=1), nLL)$par
+nLL <- make.NegLogLik(normals, c(FALSE, 2))
+optimize(nLL, c(-1, 3))$minimum
+nLL <- make.NegLogLik(normals, c(1, FALSE))
+optimize(nLL, c(1e-6, 10))$minimum
+# plotting the likelihood
+nLL <- make.NegLogLik(normals, c(1, FALSE))
+x <- seq(1.7, 1.9, len=100)
+y <- sapply(x, nLL)
+plot(x, exp(-(y - min(y))), type="l")
+nLL <- make.NegLogLik(normals, c(FALSE, 2))
+x <- seq(0.5, 1.5, len=100)
+y <- sapply(x, nLL)
+plot(x, exp(-(y -min(y))), type="l")
