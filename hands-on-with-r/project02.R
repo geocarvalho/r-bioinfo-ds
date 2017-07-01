13e1edc8dd4ef1a589514eb4d0465bcbe7f8f122
geocarvalho <geocarvalho@outlook.com>
2017-07-01 02:35:55 -0300

    it is working

--- hands-on-with-r/project02.R
+++ hands-on-with-r/project02.R
@@ -1,4 +1,4 @@
-# Atomic vectors
+# Atomic vectors!
 #types: double, integers, characters, logicals, complex and raw
 die <- c(1, 2, 3, 4, 5, 6)
 is.vector(die) #test if it is a atomic vector returning a boolean
