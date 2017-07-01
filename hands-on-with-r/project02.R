43d4534fe14d7be759cc12c04ea2a649f9b261a9
geocarvalho <geocarvalho@outlook.com>
2017-07-01 01:15:26 -0300

    chapter 4 done

--- hands-on-with-r/project02.R
+++ hands-on-with-r/project02.R
@@ -157,3 +157,55 @@ deck
 write.csv(deck, file="cards.csv", row.names=F)
 
 # R notation
+deck[1,1] # using positive integers
+deck[1,1:3] # or deck[1, c(1,2,3)]
+deck[c(1,1), c(1,2,2,3)] # to repeat
+vec <- c(6,1,3,6,10,5)
+vec[1:3]
+deck[1:2,1,drop=F]
+
+deck[-1,1:3] # negative integer exclude the item
+deck[-(2:52),1:3]
+deck[0,0] # zero create a empty object
+deck[1,] # white spaces indicate every value
+deck[1, c(TRUE, TRUE, FALSE)] # booleans means if the item have or not be showed
+rows <- c(TRUE, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F,
+          F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F,
+          F, F, F, F, F, F, F, F, F, F, F, F, F, F)
+deck[rows,]
+deck[1, c('face', 'suit')] # ask elements using names, columns are great for it
+deck[,'value']
+
+deal <- function(cards) {
+      cards[1,]
+}
+deal(deck)
+
+deck2 <- deck[1:52,] # copy, I know it isn't necessary
+deck3 <- deck[c(2,1,3:52),]
+deck3
+
+random <- sample(1:52, size=52)
+deck4 <- deck[random,]
+deck4
+
+shuffle <- function(cards){
+      random <- sample(1:52, size=52)
+      cards[random,]
+}
+deal(deck)
+deck2 <- shuffle(deck)
+deal(deck2)
+
+deck$value # $ returns the value as a vector    
+deck$face
+mean(deck$value)
+
+lst <- list(numbers=c(1,2), logical=T, strings=c('a','b','c'))
+lst[1]
+sum(lst[1]) # error
+sum(lst[[1]])
+lst$numbers
+sum(lst$numbers)
+
+# Chapter 5: Modifying Values
