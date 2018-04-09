c315882837ffe9d46bcb8664f72b1b4ffd465c72
George Carvalho <geocarvalho@outlook.com>
2018-04-09 18:16:53 -0300

    Restart the course

--- r-programing/week3/week3.R
+++
@@ -1,6 +0,0 @@
-#lapply: loop over a list and evaluate a function on each element
-# take 3 arguments: 1) a list, 2) a function 3) other arguments 
-#sapply: same as lapply but try to somplify the result
-#apply: apply a function over the margins of an array
-#tapply: apply a function over subsets of a vector
-#mapply: multivariate version of lapply 
